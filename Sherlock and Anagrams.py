e8626b9e52e55df869241abcd77711fc8c6e305c
ibfahd <ibfahd@gmail.com>
2020-10-24 15:32:33 +0100

    ok

--- Sherlock and Anagrams.py
+++ Sherlock and Anagrams.py
@@ -5,20 +5,25 @@ import os
 import random
 import re
 import sys
-
+import itertools
 # Complete the sherlockAndAnagrams function below.
+
 def sherlockAndAnagrams(s):
+    sLen = len(s)
+    res = 0
+    anagrams = []
+    for i in range(0, sLen//2):
+        for j in range(0, sLen - i):
+            anagrams.extend(list(itertools.permutations(s[j:j+i])))
+
+    for a in anagrams:
+        print(a)
+        print("".join(a))
+    return res
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     q = int(input())
-
     for q_itr in range(q):
         s = input()
-
         result = sherlockAndAnagrams(s)
-
-        fptr.write(str(result) + '\n')
-
-    fptr.close()
+        print(result)
